8082f93c83c0220b13c954e86fb2caa9d529c88d
lazzy459 <faridmusyafa212005@gmail.com>
2025-03-14 23:05:12 +0700

    Versioning

---
+++ Tugas2/celcius.js
@@ -0,0 +1,5 @@
+// 1. Konversi Fahrenheit ke Celsius
+function fahrenheitToCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9;
+}
+console.log(`Fahrenheit 100 → Celsius: ${fahrenheitToCelsius(100)}`);
